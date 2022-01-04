5458de3e62236b1aaed98e360a4d6367f908bd3a
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2022-01-04 09:58:31 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/librarySystemLight.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/librarySystemLight.dir/main.cpp.o"
+  "librarySystemLight"
+  "librarySystemLight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/librarySystemLight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+const string appName = "Library System v0.1";
+//Main ****************************************************************************************************
+int main() {
+  cout << appName << endl;
+
+  return 0;
+}
