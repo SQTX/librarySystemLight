eef495620d7f54aba27930c76bf96a5f24d7f676
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2022-01-04 11:06:15 +0100

    Library System Light(v0.2)   *Add     -Create Publication constructor     -Create Options Map     -Create printLine() function     -Create printOption() function     -Create exitApp() function     -Create new comments   *Updata     -Refactor enum const     -Edit variables name     -Edit comments

--- main.cpp
+++ main.cpp
@@ -1,66 +1,115 @@
-#include <iostream>
-using namespace std;
 //Includes & namespace*************************************************************************************
+#include <iostream>
+#include <string.h>
+#include <map>
+using namespace std;
+//Functions init *****************************************************************************************
+void printLine(string txt);
+void printOptions();
 void controlLoop();
+
+void exitApp();
+
 //Variables **********************************************************************************************
 struct Publication{
-  char[256] title;
-  char[50] author;
+  char title[256];
+  char author[50];
   int releaseDate;
-  string category;  //TODO jedne z typów (book, magazine, yearbook)
+  string category;  //TODO types (book, magazine, yearbook)
   int itemsNumber;
   int borrowersNumber;
   string lastReturnDate;
   int pagesNumber;
+
+  Publication(char *title, char *author, int releaseDate, const string &category, int itemsNumber, int borrowersNumber,
+              const string &lastReturnDate, int pagesNumber) {
+    strcpy(this->title, title);
+    strcpy(this->author, author);
+    this->releaseDate = releaseDate;
+    this->category = category;
+    this->itemsNumber = itemsNumber;
+    this->borrowersNumber = borrowersNumber;
+    this->lastReturnDate = lastReturnDate;
+    this->pagesNumber = pagesNumber;
+  }
 };
 
+//Options -----------
 enum options{
   EXIT = 0,
-  ADD_BOOK,
-  DELETE_BOOK,
-  EDIT_BOOK,
-  BORROW_BOOK,
-  RETURN_BOOK,
-  FIND_BOOK,
+  ADD_PUBLICATION,
+  DELETE_PUBLICATION,
+  EDIT_PUBLICATION,
+  BORROW_PUBLICATION,
+  RETURN_PUBLICATION,
+  FIND_PUBLICATION,
 };
+
+map<const int, string> optionMap{
+    {EXIT, "wyjscie z aplikacji"},
+    {ADD_PUBLICATION, "dodaj publikacje"},
+    {DELETE_PUBLICATION, "usun publikacje"},
+    {EDIT_PUBLICATION, "edytuj publikacje"},
+    {BORROW_PUBLICATION, "dokonaj wyporzyczenia"},
+    {BORROW_PUBLICATION, "dokonaj zwrotu"},
+    {FIND_PUBLICATION, "znajdz konkretna oublikacje"},
+};
+//App name --------------
 const string appName = "Library System v0.2";
 //Main ****************************************************************************************************
 int main() {
-  cout << appName << endl;
+  printLine(appName);
   controlLoop();  //loop switch with option
-
 
   return 0;
 }
 //Functions ************************************************************************************************
 
 void controlLoop() {
+  printOptions();
   int choice;
-  cin >> choice;
   do{
+    choice = 0; //Overwriting variable
+    cin >> choice;
     switch (choice) {
-      case ADD_BOOK:
-        addBook();
+      case ADD_PUBLICATION:
+//        addBook();
         break;
-      case DELETE_BOOK:
-        deleteBook();
+      case DELETE_PUBLICATION:
+//        deleteBook();
         break;
-      case EDIT_BOOK:
-        editBook();
+      case EDIT_PUBLICATION:
+//        editBook();
         break;
-      case BORROW_BOOK:
-        borrowBook();
+      case BORROW_PUBLICATION:
+//        borrowBook();
         break;
-      case RETURN_BOOK:
-        retrnBook();
+      case RETURN_PUBLICATION:
+//        retrnBook();
         break;
-      case FIND_BOOK:
-        findBook();
+      case FIND_PUBLICATION:
+//        findBook();
         break;
       case EXIT:
         exitApp();
         break;
-
     }
-  }while(choice != EXIT)
-};
+  }while(choice != EXIT);
+}
+void printOptions(){
+  printLine("Wybierz opcje: ");
+  for(auto &ele:optionMap){
+    string message = to_string(ele.first) + " - " + ele.second;
+    printLine(message);
+  }
+}
+
+//Exit Func.----------
+void exitApp(){
+  printLine("Koniec programu");
+}
+
+//Printer----------
+void printLine(string txt){
+  cout << txt << endl;
+}
